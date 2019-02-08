b3767c1a8c17ca34064d1dbb7a3bbd1eb6420f43
Bazzan <sebake01@gmail.com>
2019-02-08 15:57:05 +0100

    Tree klar

--- src/tree/BinarySearchTreeNode.java
+++ src/tree/BinarySearchTreeNode.java
@@ -26,10 +26,10 @@ package tree;
  * 
  * @param <T>
  */
-//@SuppressWarnings("unused") // Denna rad ska plockas bort. Den finns här
-							// tillfälligt för att vi inte ska tro att det är
-							// fel i koden. Varningar ska normalt inte döljas på
-							// detta sätt, de är (oftast) fel som ska fixas.
+// @SuppressWarnings("unused") // Denna rad ska plockas bort. Den finns här
+// tillfälligt för att vi inte ska tro att det är
+// fel i koden. Varningar ska normalt inte döljas på
+// detta sätt, de är (oftast) fel som ska fixas.
 public class BinarySearchTreeNode<T extends Comparable<T>> {
 
 	public T data;
@@ -38,146 +38,181 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
 
 	public BinarySearchTreeNode(T data) {
 		this.data = data;
-		
-		
+
 	}
 
 	public boolean add(T data) {
 		int comp = data.compareTo(this.data);
-		
-		if(this.data == data) {
-			return false;			
-		}else if(comp < 0) {
-			if(left == null) {
+
+		if (this.data == data) {
+			return false;
+		} else if (comp < 0) {
+			if (left == null) {
 				left = new BinarySearchTreeNode(data);
 				return true;
-			}else {
+			} else {
 				return left.add(data);
 			}
-			
-		}else if(comp > 0) {
-			if(right == null) {
+
+		} else if (comp > 0) {
+			if (right == null) {
 				right = new BinarySearchTreeNode(data);
 				return true;
-			}else
+			} else
 				return right.add(data);
-		}else 
+		} else
 			return false;
-				
+
 	}
 
 	private T findMin() {
-		if(this.data == null) {
+		if (this.data == null) {
 			return null;
-		}else if(left == null) {
-			return data;
-		}return left.findMin();
-		
-		
-		
-//		return null;
+		} else if (left == null) {
+			return this.data;
+		}
+		return left.findMin();
+
+		// return null;
 	}
 
 	public BinarySearchTreeNode<T> remove(T data) {
-		if(this.data == null) {
-			return this;
-		}
-		
-		int comp = data.compareTo(this.data);
-		
-//		if (data == this.data) {
-//			return this.data = null;
-//		
-//		}
-		if(comp < 0) {
-			 return left.remove(data);
-		
-		}else if(comp > 0) {	
-			return right.remove(data);
-		
-		}else if(left != null && right != null) {	
-			this.data = right.findMin();
-			right.remove(this.data);
-
-		}else
-			this.data = (left != null ) ? left.remove(data) : right.remove(data);
+		if (this.data == null) {
 			return null;
-		
-			
-//		}else if(left == this) {
-//
-//			left = ( left != null ) ?  left : right;
-//		
-//		}else if(right == this) {
-//			right = ( left != null) ? left : right;
+		}
+
+//		if (data == this.data) {
+//			return this;
 //		}
-//		System.out.println(data);
-//		return null;
+
+		System.out.println("remove size " + size());
+		int comp = data.compareTo(this.data);
+
+		if (comp < 0) {
+			if(left == null) {
+				return this;
+			}
+			left = left.remove(data);
+
+		} else if (comp > 0) {
+			 if(right == null) {
+				 return this;
+			 }
+			right = right.remove(data);
+
+		} else {
+			if (left != null && right != null) {
+				this.data = this.right.findMin();
+				this.right = this.right.remove(this.data);
+
+			} else {
+				return (left != null) ? left : right;
+			}
+		}
+		return this;
+		// }else if (this.left == this) {
+		// this.left = (left != null) ? left : right;
+		//
+		// }else if (this.right == this) {
+		// this.right = (left != null) ? left : right;
 	}
+
+	// } else if (left != null) {
+	// return left;
+	// } else {
+	// return right;
+
+	// else if(left == null && right == null) {
+	// return this;
+	// }
+
+	// }
+	// return null;
+	// }
+
+	// return null;
+
+	// (left == null ) {
+	// return right.remove(data);
+	// }else if(right == null){
+	// return left.remove(data);
+	// }else {
+	// return this;
+	// }
+
+	// }else if(left == this) {
+	//
+	// left = ( left != null ) ? left : right;
+	//
+	// }else if(right == this) {
+	// right = ( left != null) ? left : right;
+	// }
+	// System.out.println(data);
+	// return null;
 
 	public boolean contains(T data) {
 		int comp = data.compareTo(this.data);
-		
-		if(data == this.data) {
+
+		if (data == this.data) {
 			return true;
-		}else if(comp < 0) {
-			if(left == null) {
+		} else if (comp < 0) {
+			if (left == null) {
 				return false;
-			}else {
+			} else {
 				return left.contains(data);
 			}
-		}else if(comp > 0) {
-			if(right == null) {
+		} else if (comp > 0) {
+			if (right == null) {
 				return false;
-			}else {
+			} else {
 				return right.contains(data);
 			}
 		}
-		
+
 		return false;
 	}
 
 	public int size() {
 		int size = 1;
-		
-		if(right == null && left == null) {
-			return size =+ 1;
-		}else if(left != null && right == null) {
-			return size =+ left.size() + 1;
-		}else if(right != null && left == null) {
-			return size =+ right.size() + 1;
-		}else if (left != null && right != null) {
-			return size =+left.size() + right.size() + 1;
-		}else
-		System.out.println(size);
+
+		if (right == null && left == null) {
+			return size = +1;
+		} else if (left != null && right == null) {
+			return size = +left.size() + 1;
+		} else if (right != null && left == null) {
+			return size = +right.size() + 1;
+		} else if (left != null && right != null) {
+			return size = +left.size() + right.size() + 1;
+		} else
+			System.out.println(size);
 		return size;
 	}
 
 	public int depth() {
-		if(this.data == null) {
+		if (this.data == null) {
 			return -1;
-			}
+		}
 		int lDepth = 0;
 		int rDepth = 0;
-		
-		if(left != null) {
+
+		if (left != null) {
 			lDepth = left.depth() + 1;
-		}else if(right != null ) {
+		} else if (right != null) {
 			lDepth = right.depth() + 1;
 		}
 
-		if(right != null) {
+		if (right != null) {
 			rDepth = right.depth() + 1;
-		}else if(left != null) {
-			rDepth = left.depth() +1;
+		} else if (left != null) {
+			rDepth = left.depth() + 1;
 		}
-		
-		if(rDepth < lDepth) {
+
+		if (rDepth < lDepth) {
 			return lDepth;
-		}else {
+		} else {
 			return rDepth;
 		}
-		
+
 	}
 
 	public String toString() {
@@ -186,9 +221,9 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
 			str = str + left.toString() + ", ";
 		}
 		str = str + data;
-		
-		if(right != null){
-			str = str +  ", " + right.toString();
+
+		if (right != null) {
+			str = str + ", " + right.toString();
 		}
 		return str;
 	}
