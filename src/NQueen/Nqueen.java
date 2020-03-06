c638c841f9078234c3f853f10576c4d2a91a70f8
Bazzan <sebake01@gmail.com>
2020-03-06 11:42:18 +0100

    Update Nqueen.java

--- src/NQueen/Nqueen.java
+++ src/NQueen/Nqueen.java
@@ -1,64 +1,93 @@
-package NQueen;
+//Authors:
+//Disa Röed Sahlström
+//Sebastian Åkerlund
+
+package ntest;
 
 public class Nqueen {
 
 	
 	private final int n = 8;
-	
-	private static int board[][] = {
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0}};
-	
+	private static int chessB[][] = new int [8][8];
 
-	
-	public void printBoard(int board[][]) {
+	// Prints the chessboard
+	public void printChessB(int chessB[][]) {
 		System.out.print("  ");
 		for(int col = 1; col <= n ; col++) {
-			System.out.print(" " + col+ " " );
+			System.out.print(" " + col + " " );
 		}
 		System.out.println();
 
 		for(int row = 0; row < n; row++) {
 			System.out.print(row + 1 + "|");
 			
-			for (int col = 0 ; col < n ; col++) {
+			for (int coll = 0 ; coll < n ; coll++) {
 
-				System.out.print( " "+ board[row][col] + " " );
+				System.out.print( " "+ chessB[row][coll] + " " );
 				
-			}
-			
-				
+			}		
 			System.out.println();
 		}
+	}
+	
+	
+	// Checks if attacking other queens is possible
+	public boolean attack(int chessB[][], int row , int coll) {	
+		for(int i = 0 ; i < coll; i++) {
+			if(chessB[row][i] == 1) {
+				return false;
+			}
+		}
 		
-	}
-	
-	
-	// ska kolla om den kan attakera andra damer
-	public boolean attack(int board[][], int row , int col) {
-		return false;
-	}
-	
-	// ska lösa problemet rekursivt/backtracking
-	public boolean placeQ(int board[][], int row, int col) {
+		
+		for(int i = row, k = coll; k>=0 && i <n; i++, k--) {
+			if(chessB[i][k] == 1) {
+				return false;
+			}
+		}
+		
+		for(int i = row, k = coll; i >= 0 && k >= 0; i--, k-- ) {
+			if(chessB[i][k]== 1) {
+				return false;
+			}
+		}
+		
 		return true;
 	}
 	
-	
+	// Recursively checks for placement of queen
+	public boolean placeQ(int chessB[][], int coll) {
+		if(coll == n) {
+			return true;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			
+			if(attack(chessB, i, coll)) {
+				chessB[i][coll] = 1;
+				
+				if(placeQ(chessB, coll + 1) == true) {
+					return true;
+				}
+				chessB[i][coll] = 0;
+			}
+		}
+		return false;
+	}
+	//Draws chessboard if queens can be placed appropriately
+	public boolean run() {	
+		if(placeQ(chessB, 0) == true) {
+			printChessB(chessB);
+			return true;
+		}
+		return false;
+	}
 	
 	
 	public static void main(String args[]) {
 		
 		Nqueen nQueen = new Nqueen();
-		nQueen.printBoard(board);
-		
-		
-		
+		nQueen.run();
+				
 	}
 }
